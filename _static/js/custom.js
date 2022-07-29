2556756d837740aa12195dffbfd968cda5a58d95
mluerig <moritz.luerig@gmail.com>
2022-07-29 12:04:40 +0200

    - auto-generated with sphinx -

---
+++ _static/js/custom.js
@@ -0,0 +1,28 @@
+(function() {
+	
+  if (!location.hostname === "localhost" || !location.hostname === "127.0.0.1") {
+	  if (!location.href.endsWith('/')) {
+		window.location = location.href + '/'
+	  }
+  }
+}())
+
+ function download_file(filelink) {
+	 
+	 	var parts = filelink.split('/');
+		var filename = parts.pop() || parts.pop();  // handle potential trailing slash
+	 
+     var req = new XMLHttpRequest();
+     req.open("GET", filelink, true);
+     req.responseType = "blob";
+     req.onload = function (event) {
+         var blob = req.response;
+         var fileName = req.getResponseHeader("fileName") //if you have the fileName header available
+         var link=document.createElement('a');
+         link.href=window.URL.createObjectURL(blob);
+         link.download=filename;
+         link.click();
+     };
+
+     req.send();
+ }
